c3839e286996580dd0e00a56fb230db0bf5875af
Stanislav Katkov <skatkov@cheddar.me>
2023-02-21 17:38:48 +0100

    Create unlogged table for PG cache

--- lib/active_support/cache/database_store/migration.rb
+++ lib/active_support/cache/database_store/migration.rb
@@ -11,6 +11,24 @@ module ActiveSupport
             t.timestamp :created_at, null: false, index: true
             t.timestamp :expires_at, index: true
           end
+
+          if postgresql?
+            ActiveRecord::Base.connection.execute("ALTER TABLE activesupport_cache_entries SET UNLOGGED")
+          end
+        end
+
+        private
+
+        def adapter
+          if ActiveRecord::VERSION::STRING.to_f >= 6.1
+            ActiveRecord::Base.connection_db_config.adapter.to_s
+          else
+            ActiveRecord::Base.connection_config[:adapter].to_s
+          end
+        end
+
+        def postgresql?
+          adapter =~ /postg/
         end
       end
     end
