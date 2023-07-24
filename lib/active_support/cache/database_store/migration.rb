63f62fc02b994fba28ad86e51b47f903ce6d286e
Stanislav Katkov <skatkov@cheddar.me>
2023-07-24 11:26:31 +0200

    Use partial index for expires_at column

--- lib/active_support/cache/database_store/migration.rb
+++ lib/active_support/cache/database_store/migration.rb
@@ -9,8 +9,30 @@ module ActiveSupport
             t.binary :value, null: false
             t.string :version, index: true
             t.timestamp :created_at, null: false, index: true
-            t.timestamp :expires_at, index: true
+            t.timestamp :expires_at
           end
+
+          if mysql?
+            # MySQL and MariaDB don't support partial indexes
+            add_index :activesupport_cache_entries, :expires_at
+          else
+            # For Sqlite3, PostgreSQL we use partial index, because expires_at column can be null it would be wasteful to include it in index
+            add_index :activesupport_cache_entries, :expires_at, where: 'expires_at IS NOT NULL'
+          end
+        end
+
+        private
+
+        def adapter
+          if ActiveRecord::VERSION::STRING.to_f >= 6.1
+            ActiveRecord::Base.connection_db_config.adapter.to_s
+          else
+            ActiveRecord::Base.connection_config[:adapter].to_s
+          end
+        end
+
+        def mysql?
+          adapter =~ /mysql/
         end
       end
     end
