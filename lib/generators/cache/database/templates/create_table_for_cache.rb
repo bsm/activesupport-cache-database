65a7a75155e0fb6ae4a5a89e3214f01656d5d59f
Stanislav Katkov <skatkov@cheddar.me>
2023-08-25 15:54:28 +0200

    fixing specs

--- lib/generators/cache/database/templates/create_table_for_cache.rb
+++ lib/generators/cache/database/templates/create_table_for_cache.rb
@@ -1,4 +1,4 @@
-class Migration < ActiveRecord::Migration[5.2]
+class CreateTableForCache < ActiveRecord::Migration[5.2]
   def change
     create_table :activesupport_cache_entries, primary_key: 'key', id: :binary, limit: 255 do |t|
       t.binary :value, null: false
