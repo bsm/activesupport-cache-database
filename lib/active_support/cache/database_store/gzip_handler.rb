5496178b104a0f4039de4f163aa44c8b43fe011e
Stanislav Katkov <skatkov@cheddar.me>
2023-09-22 17:18:17 +0200

    cleanup

--- lib/active_support/cache/database_store/gzip_handler.rb
+++
@@ -1,9 +0,0 @@
-module ActiveSupport::Cache::DatabaseStore::GzipHandler
-  def self.compress(object)
-    ActiveSupport::Gzip.compress(Marshal.dump(object))
-  end
-
-  def self.decompress(bytes)
-    Marshal.load(ActiveSupport::Gzip.decompress(bytes))
-  end
-end
--- lib/active_support/cache/database_store/plain_handler.rb
+++
@@ -1,9 +0,0 @@
-module ActiveSupport::Cache::DatabaseStore::PlainHandler
-  def self.compress(object)
-    Marshal.dump(object)
-  end
-
-  def self.decompress(bytes)
-    Marshal.load(bytes)
-  end
-end
